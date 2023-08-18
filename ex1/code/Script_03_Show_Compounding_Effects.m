381a95c7c6eb709eab3bbc680b7669ebba56f2bb
donghui.xu@pnnl.gov <xudo627@we48331.pnl.gov>
2023-08-18 11:30:54 -0700

    add code

--- ex1/code/Script_03_Show_Compounding_Effects.m
+++ ex1/code/Script_03_Show_Compounding_Effects.m
@@ -8,10 +8,29 @@ thres  = 1000 : 1000 : 50000;
 
 prefix = 'Turning_30m_SR';
 
-if ~exist('../data/inundCF.mat','file')
+if exist('../data/inundCF.mat','file')
+    load('../data/inundCF.mat')
+else
     [inundf,inundv,hdiff,inunds] = read_CF(Output, thres);
     save('../data/inundCF.mat','inunds','inundf','inundv','hdiff','-v7.3');
 end
+
+figure(4);
+
+for i = 1 : 16
+    subplot(4,4,i)
+    tmp1 = sum(inundv(i,:,:,1),2);
+    tmp2 = sum(inundv(i,:,:,2),2);
+    plot(tmp1(:),'r-','LineWidth',2); hold on;
+    plot(tmp2(:),'b-','LineWidth',2);
+    plot(tmp1(:)-tmp2(:),'-','LineWidth',2); hold on; grid on;
+end
+
+figure(5);
+tmp1 = sum(inundv(:,:,:,1),2);
+tmp2 = sum(inundv(:,:,:,2),2);
+tmp  = tmp2 - tmp1;
+
 
 
 
